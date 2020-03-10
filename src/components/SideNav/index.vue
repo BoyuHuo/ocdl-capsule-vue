c3384618fd8aef3c35de6c1ff7d76d12cc59494f
blue <boyu.huo.china@gmail.com>
2020-03-10 18:51:24 -0400

    nav fix

--- src/components/SideNav/index.vue
+++ src/components/SideNav/index.vue
@@ -1,26 +1,29 @@
 <template>
   <div>
-    <el-menu mode="vertical" :show-timeout="200" :collapse="isCollapse" :unique-opened="true">
-      <el-menu-item index="2">
-        <hamburger
-          class="hamburger-container toggle-btn"
-          :toggleClick="toggleSideBar"
-          :isActive="sidebar.opened"
-        ></hamburger>
-      </el-menu-item>
-      <el-menu-item index="1">
-        <i class="el-icon-document"></i>
-        <span slot="title">Code Template</span>
-      </el-menu-item>
-      <el-menu-item index="2">
-        <i class="el-icon-receiving"></i>
-        <span slot="title">User Storage</span>
-      </el-menu-item>
-      <el-menu-item index="3">
-        <i class="el-icon-pie-chart"></i>
-        <span slot="title">Model Center</span>
-      </el-menu-item>
-    </el-menu>
+    <div style="height:100%; background-color: #e6e6e6; position: fixed"  >
+      <el-menu  mode="vertical" :show-timeout="200" :collapse="isCollapse" :unique-opened="true">
+        <el-menu-item index="2">
+          <hamburger
+            class="hamburger-container toggle-btn"
+            :toggleClick="toggleSideBar"
+            :isActive="sidebar.opened"
+          ></hamburger>
+        </el-menu-item>
+        <el-menu-item index="1">
+          <i class="el-icon-document"></i>
+          <span slot="title">Code Template</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-receiving"></i>
+          <span slot="title">User Storage</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-pie-chart"></i>
+          <span slot="title">Model Center</span>
+        </el-menu-item>
+      </el-menu>
+    </div>
+
   </div>
 </template>
 
