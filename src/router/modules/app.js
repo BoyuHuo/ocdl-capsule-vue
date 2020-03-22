e29a74efaa5c11dd544d028572ba5858affa5539
BoyuHuo <boyu.huo.china@gmail.com>
2020-03-22 19:41:41 -0400

    update router

--- src/router/modules/app.js
+++ src/router/modules/app.js
@@ -7,26 +7,11 @@ const appModule = [
     component: () => import(/* webpackChunkName: "app-index" */ '@/views/login/index')
   },
   {
-    name: 'register',
-    meta: {
-      title: '注册'
-    },
-    component: () => import(/* webpackChunkName: "app-index" */ '@/views/register/index')
-  },
-  {
-<<<<<<< HEAD
     name: 'home',
     meta: {
       title: '主页'
     },
     component: () => import(/* webpackChunkName: "app-index" */ '@/views/home/index')
-=======
-    name: 'landing',
-    meta: {
-      title: '主页'
-    },
-    component: () => import(/* webpackChunkName: "app-index" */ '@/views/landing/index')
->>>>>>> c3384618fd8aef3c35de6c1ff7d76d12cc59494f
   }
 ]
 export default appModule
